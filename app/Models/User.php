48edca8a222186f7adfb2a454aae7efff7989bde
Arman Darmesh <darmesharman@gmail.com>
2022-04-20 15:08:30 +0600

    Authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'last_name',
+        'first_name',
+        'username',
         'password',
     ];
 
